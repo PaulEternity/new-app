38d490b9c1a0b7416bdbec8abc8358538fba41e5
Paul <14447139+PaulEternity@user.noreply.gitee.com>
2026-01-19 15:31:02 +0800

    init

---
+++ src/main/java/com/paul/appgen/AppGenApplication.java
@@ -0,0 +1,13 @@
+package com.paul.appgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paul/appgen/AppGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.appgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
